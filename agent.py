b7e0e40bc46b28e2f401aa00782cfbcbcac67039
gokul-1998 <gokulakrishnanm1998@gmail.com>
2025-05-24 08:47:49 +0530

    bug at 23.42

---
+++ agent.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from livekit.agent import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm
+)
+from livekit.agents.multimodal import (
+    MultimodalAgent,
+   
+)
+from livekit.plugins import openai
+from dotenv import load_dotenv
+from api import AssistantFnc
+from prompts import WELCOME_MESSAGE,INSTRUCTIONS
+import os
+
+load_dotenv()
+
+async def entrypoint(ctx: JobContext):
+    # Create a multimodal agent
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+    await ctx.wait_for_participants()
+
+
+    model=openai.realtime.RealtimeModel(
+        instructions=INSTRUCTIONS,
+        voice="shimmer",
+        temperature=0.8,
+        modalities=["text", "audio"],
+
+    )
+    assistant_fnc=AssistantFnc()
+    assistant=MultimodalAgent(model=model, fnc_ctx=assistant_fnc)
+    assistant.start(ctx.room)
+
+    session=model.sessions[0]
+
+    session.conversation.item.create(
+        llm.ChatMessage(
+            role="assistant",
+            content=WELCOME_MESSAGE,
+        )
+    )
+    session.response.create()
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
